bd459f4b8cbe7c55b66fec1c5f5014c5e3251885
Daniel-Musunza <musunzafestus2019@gmail.com>
2024-09-05 09:42:20 +0300

    download

--- src/components/marketing/account-settings/Certificate.js
+++ src/components/marketing/account-settings/Certificate.js
@@ -1,64 +1,74 @@
 // import node module libraries
-import React, { useState, useEffect, Fragment } from 'react';
-import { useQuery } from 'react-query';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { useSelector } from 'react-redux';
-
-// import QRCode component
+import React, { Fragment, useRef } from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 // import layouts and assets
 import NavbarMegaMenu from 'layouts/marketing/navbars/mega-menu/NavbarMegaMenu';
-import courseService from '../../dashboard/features/courses/courseService';
 import CertificateTemplate from 'assets/images/certificate/certificate.png';
 
 const Certificate = () => {
-  const { user } = useSelector((state) => state.auth);
-  const token = user?.data?.accessToken;
-  const studentData = JSON.parse(localStorage.getItem('studentData'));
+	const certificateRef = useRef(null);
 
-  const certificateDetails = {
-	id: "123",
-    name: 'Test Student',
-    certificationDate: 'July 2, 2024',
-    certificationTitle: 'Business Management- Startups'
-  };
+	const certificateDetails = {
+		id: "123",
+		name: 'Test Student',
+		certificationDate: 'July 2, 2024',
+		certificationTitle: 'Business Management- Startups'
+	};
 
-  const  verificationLink= `https://emergelms.vercel.app/certificate/${certificateDetails.name}/${certificateDetails.id}`;
+	const verificationLink = `https://emergelms.vercel.app/certificate/${certificateDetails.name}/${certificateDetails.id}`;
 
-  return (
-    <Fragment>
-      <NavbarMegaMenu />
-      <div className='certificate-container'>
-        <div className='certificate'>
-          <img src={CertificateTemplate} alt="Certificate" />
+	// Function to download the certificate as a PDF
+	const downloadPDF = () => {
+		const certificateElement = certificateRef.current;
 
-          <div className='certification-date'>
-            <span>Issued </span>
-            <p>{certificateDetails.certificationDate}</p>
-          </div>
+		html2canvas(certificateElement).then((canvas) => {
+			const imgData = canvas.toDataURL('image/png');
+			const pdf = new jsPDF('landscape');
+			pdf.addImage(imgData, 'PNG', 10, 10, 280, 190);
+			pdf.save(`${certificateDetails.name}-certificate.pdf`);
+		});
+	};
 
-          <div className='certified-name'>
-            <h2>{certificateDetails.name}</h2>
-          </div>
-          <div className='certification'>
-            <h2>{certificateDetails.certificationTitle}</h2>
-          </div>
+	return (
+		<Fragment>
+			<NavbarMegaMenu />
+			<div className="download">
+				<button onClick={downloadPDF} style={{ border: 'none', borderRadius: '5px', textDecoration: 'none', color: '#fff', backgroundColor: '#754FFE' }}>Download as PDF</button>
+			</div>
+			<div className='certificate-container'>
+				
+				<div className='certificate' ref={certificateRef}>
+					<img src={CertificateTemplate} alt="Certificate" />
 
-          <div className='qr-code'>
-            <QRCodeCanvas value={verificationLink} size={80} />
-          </div>
+					<div className='certification-date'>
+						<span>Issued </span>
+						<p>{certificateDetails.certificationDate}</p>
+					</div>
 
-          <div className='verify-link'>
-            <a href={verificationLink}>
-              Verify this certification at {verificationLink}
-            </a>
-          </div>
-        </div>
-      </div>
-    </Fragment>
-  );
+					<div className='certified-name'>
+						<h2>{certificateDetails.name}</h2>
+					</div>
+					<div className='certification'>
+						<h2>{certificateDetails.certificationTitle}</h2>
+					</div>
+
+					<div className='qr-code'>
+						<QRCodeCanvas value={verificationLink} size={80} />
+					</div>
+
+					<div className='verify-link'>
+						<a href={verificationLink}>
+							Verify this certification at {verificationLink}
+						</a>
+					</div>
+				</div>
+
+			</div>
+		</Fragment>
+	);
 };
 
 export default Certificate;
